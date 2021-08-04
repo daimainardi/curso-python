da595cc89653ae8d29ee4cffa0adadfe91ec931b
Daiane Mainardi <daimainardi@hotmail.com>
2021-08-04 12:50:42 -0300

    primeiro commit

---
+++ aula4/desafio3.py
@@ -0,0 +1,9 @@
+# numero1 = int(input("Primeiro número = "))
+# numero2 = int(input("Segundo número = "))
+# soma = numero1 + numero2
+# print(soma)
+
+numero1 = int(input("Primeiro número = "))
+numero2 = int(input("Segundo número = "))
+print(numero1 + numero2)
+
